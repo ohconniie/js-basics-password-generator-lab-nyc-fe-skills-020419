d3ad7545e152411aef544900c802db54475f5fe1
Connie Chan <conniechan.10@gmail.com>
2019-04-01 23:43:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", () => {
   //start your code here
+  
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+  
+  function onSubmit(event) {
+    event.preventDefault()
+    const input = document.getElementById('passwordlength')
+    console.log(input.value)
+  }
+  
+  document.addEventListener('submit', onSubmit)
 });
